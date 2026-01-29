0ffd69632af9e028edb04bf606ce69cd4ed15606
Kamil Szymczak <kamilkordianszymczak@gmail.com>
2026-01-29 19:58:53 +0100

    Remove mods

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 mod note;
 mod scale;
-mod signal;
-mod signals;
 mod utils;
-mod scale;
-mod note;
 
 mod superdirt;
 mod pattern;
@@ -23,13 +19,8 @@ fn main() {
     println!("{}", pattern::display_pattern(&lang::eval_pattern(parser::parse("cat([1, 2])").unwrap()).unwrap()));
     let pat1 = fastcat([s("bd"), slowcat([s("sn"), s("hh")].into_iter()).boxed()].into_iter()).boxed();
     let pat2 = pattern::cycled(ControlMessage::sound("arpy")).boxed();
-
-    let code = r#"s(cat(["bd", cat(["sn", "hh"])]))"#;
-    let parsed = parser::parse(code).unwrap();
-    let pat3 = lang::eval_control_pattern(parsed).unwrap();
     run_server(vec![
         pat1,
         pat2,
-        pat3,
     ].into_iter());
 }
